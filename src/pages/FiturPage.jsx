c602386ce1941c80986bf35cb9fea98de40bce7e
HelmiITK <helmi220602@gmail.com>
2024-03-05 02:45:16 +0800

    first commit

---
+++ src/pages/FiturPage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+const FiturPage = () => {
+   return (
+      <>
+         <Navbar/>
+         <div>FiturPage</div>
+      </>
+   )
+}
+
+export default FiturPage
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+const LoginPage = () => {
+   return (
+      <div>
+         <Navbar />
+         <div>LoginPage</div>
+      </div>
+   )
+}
+
+export default LoginPage
