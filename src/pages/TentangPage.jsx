4817b8dcc3827a6ee09673782a57b90e790d5fae
MasRangga <ranggah503@gmail.com>
2024-03-13 01:15:39 +0800

    membuat menjadi responsive

--- src/pages/TentangPage.jsx
+++ src/pages/TentangPage.jsx
@@ -8,25 +8,25 @@ const TentangPage = () => {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto">
+      <div className="container pt-20 md:pt-44 mx-auto">
         <div className="text-center">
-          <div className="text-5xl font-semibold mb-5 text-blue-900">
+          <div className="text-3xl md:text-5xl font-semibold mb-5 text-blue-900">
             Tentang Kami
           </div>
-          <p className="text-2xl font-medium text-gray-400">
+          <p className="text-lg md:text-2xl font-medium text-gray-400">
             Tingkatkan Efisien Global dengan Meraih
           </p>
         </div>
 
-        <div className="flex mt-10">
-          <div className="flex flex-col justify-center mr-10">
-            <div className="text-5xl font-semibold text-blue-900">
+        <div className="flex flex-col md:flex-row md:mt-28">
+          <div className="flex flex-col justify-center md:mr-40">
+            <div className="text-3xl md:text-5xl font-semibold text-blue-900">
               WHO WE ARE
             </div>
-            <p className="py-3 text-lg">
+            <p className="py-3 text-base md:text-lg">
               Selamat datang di MERAIH, Aplikasi HR yang memudahkan bisnis Anda!
             </p>
-            <p className="py-3 text-lg">
+            <p className="py-3 text-base md:text-lg">
               Kami membuat manajemen tim global menjadi lebih mudah dan
               membebaskan bisnis dari kompleksitas. Dengan Meraih, Anda dapat
               dengan mudah mengontrak dan mempekerjakan karyawan di seluruh
@@ -34,34 +34,38 @@ const TentangPage = () => {
               menyederhanakan pengelolaan HR untuk tim global Anda dan
               mengonsolidasikan proses penggajian
             </p>
-            <p className="py-3 text-lg">
+            <p className="py-3 text-base md:text-lg">
               Membuat Manajemen Tim Global Tidak Lagi Menjadi Hambatan, Tapi
               Menjadi Peluang untuk Pertumbuhan Bisnis yang Tidak Terbatas!
             </p>
           </div>
-          <img className="mt-2" src={GambarAbout} alt="GambarAbout" />
+          <img className="mt-2 md:mt-0" src={GambarAbout} alt="GambarAbout" />
         </div>
-        <div className="card w-full shadow-xl rounded-xl p-5 mt-10 bg-blue-100 flex items-center">
-          <div className="flex-none mr-5">
-            <img src={Gambar3} alt="Gambar3" />
-          </div>
-          <div className="card-content flex-1">
-            <div className="card-title">
-              <h2 className="text-5xl text-blue-900 font-bold">
-                Aplikasi Meraih merupakan satu solusi untuk semua kebutuhan HR
-                Anda
-              </h2>
+
+        <div className="card w-full shadow-xl rounded-xl p-5 my-20 bg-blue-100 flex flex-col md:flex-row items-center">
+          <div className="card-content md:flex-1">
+            <div className="flex flex-col md:flex-row items-center">
+              <img src={Gambar3} alt="Gambar3" className="md:mr-5" />
+              <div className="mx-0 md:mx-5">
+                <div className="card-title">
+                  <h2 className="text-lg md:text-5xl text-blue-900 font-bold">
+                    Aplikasi Meraih merupakan satu solusi untuk semua kebutuhan
+                    HR Anda
+                  </h2>
+                </div>
+                <p className="text-base md:text-lg text-gray-400 mt-5">
+                  Optimalkan pengelolaan operasi HR Anda dengan bantuan solusi
+                  terintegrasi dari Meraih.
+                </p>
+              </div>
             </div>
-            <p className="text-base text-gray-400 mt-5">
-              Optimalkan pengelolaan operasi HR Anda dengan bantuan solusi
-              terintegrasi dari Meraih.
-            </p>
+            <button className="flex gap-1 items-center border-2 border-primary rounded-3xl py-2 px-4 bg-primary text-white hover:bg-blue-900 hover:border-blue-900 duration-300 lg:gap-2 lg:text-xl lg:font-medium lg:py-3 lg:px-6 mt-4 md:mt-0 ml-auto">
+              <p>Hubungi Sales</p>
+              {/* <FaAngleRight className="w-5 h-5 lg:w-6 lg:h-6" /> */}
+            </button>
           </div>
-          <div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Tombol
-          </button>
         </div>
+
         <Footer />
       </div>
     </>
