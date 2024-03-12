fa8db5f2fe88b412f39968d2a6d6d4cb38c7cd72
MasRangga <ranggah503@gmail.com>
2024-03-13 01:24:20 +0800

    first commit

--- src/pages/FiturPage.jsx
+++ src/pages/FiturPage.jsx
@@ -2,11 +2,21 @@ import Navbar from "../components/Navbar"
 
 const FiturPage = () => {
    return (
-      <>
-         <Navbar />
-         <div className="pt-48">FiturPage</div>
-      </>
-   )
+     <>
+       <Navbar />
+       <div className="container pt-20 md:pt-44 mx-auto">
+         <div className="text-center">
+           <div className="text-3xl md:text-5xl font-semibold mb-5 text-blue-900">
+             Nikmati pengalaman terbaik mengelola HR dan Bisnis Anda
+           </div>
+           <p className="text-lg md:text-2xl font-medium text-gray-400">
+             Dapatkan Akses ke Fitur dan Layanan Sesuai Keinginan Anda
+           </p>
+         </div>
+         
+       </div>
+     </>
+   );
 }
 
 export default FiturPage
