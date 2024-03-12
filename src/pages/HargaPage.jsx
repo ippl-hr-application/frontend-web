a40a12041262cf3ad418c671878cb0c9ce1b2bad
MasRangga <ranggah503@gmail.com>
2024-03-13 01:27:00 +0800

    first commit

--- src/pages/HargaPage.jsx
+++ src/pages/HargaPage.jsx
@@ -2,11 +2,21 @@ import Navbar from "../components/Navbar"
 
 const HargaPage = () => {
    return (
-      <>
-         <Navbar />
-         <div>HargaPage</div>
-      </>
-   )
+     <>
+       <Navbar />
+       <div className="container pt-20 md:pt-44 mx-auto">
+         <div className="text-center">
+           <div className="text-3xl md:text-5xl font-semibold mb-5 text-blue-900">
+             Satu Solusi HR untuk Memenuhi Kebutuhan Operasional dan Strategis
+             Bisnis Anda dengan Sempurna
+           </div>
+           <p className="text-lg md:text-2xl font-medium text-gray-400">
+             Dapatkan Penawaran Terbaik yang Sesuai dengan Kebutuhan Bisnis Anda
+           </p>
+         </div>
+       </div>
+     </>
+   );
 }
 
 export default HargaPage
