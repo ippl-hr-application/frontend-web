1e3854434f7d9d60e8edea1304b0d37980d3834d
MasRangga <ranggah503@gmail.com>
2024-03-13 12:18:11 +0800

    update borderbox

--- src/pages/FiturPage.jsx
+++ src/pages/FiturPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { BsFillCheckCircleFill } from "react-icons/bs"; 
-import GambarMainSaction from "../assets/GambarMainSaction.png";
+import GambarFitur from "../assets/GambarFitur.png";
 import { ImWhatsapp } from "react-icons/im";
 import { FaAngleRight } from "react-icons/fa";
 
@@ -12,47 +12,50 @@ const FiturPage = () => {
         <BsFillCheckCircleFill className="text-green-400 bg-black w-6 h-6 border-2 border-green-900 rounded-full" />
       ),
       title:
-        "Optimalkan lebih dari 80% kapasitas HR untuk administrasi dan operasional.",
+        "Memudahkan seluruh operasional dan administrasi HR sehari-hari dengan sistem terotomasi.",
     },
     {
       icon: (
         <BsFillCheckCircleFill className="text-green-400 bg-black w-6 h-6 border-2 border-green-900 rounded-full" />
       ),
-      title: "Monitoring progress onboarding dengan Lebih Teliti dan Efektif.",
+      title:
+        "Mengelola payroll, absensi, kehadiran, performance, hingga benefit karyawan secara fleksibel, akurat, dan aman.",
     },
     {
       icon: (
         <BsFillCheckCircleFill className="text-green-400 bg-black w-6 h-6 border-2 border-green-900 rounded-full" />
       ),
-      title: "Rancang alur kerja yang transparan dan terstruktur.",
-    },
-    {
-      icon: (
-        <BsFillCheckCircleFill className="text-green-400 bg-black w-6 h-6 border-2 border-green-900 rounded-full" />
-      ),
-      title: "Dokumentasikan komponen offboarding dalam satu dashboard.",
+      title:
+        "Menciptakan budaya terstruktur dan mengembangkan potensi karyawan dengan dashboard analisis komprehensif.",
     },
   ];
 
   return (
     <>
       <Navbar />
-      <div className="container pt-20 md:pt-44 mx-auto">
-        <div className="relative pt-4 bg-gradient-to-r from-blue-300 shadow-2xl mb-10 md:pt-6 lg:pt-4">
-          <div className="mx-4 pb-36 md:flex md:flex-col md:justify-center md:items-center lg:pb-0 lg:flex lg:flex-row-reverse lg:gap-4 lg:mt-2">
+      <div className="container pt-20 mx-auto">
+        <div className="relative bg-gradient-to-r from-blue-300 shadow-2xl mb-10 md:pt-6 lg:pt-4">
+          <div className="text-center mt-24">
+            <div className="text-3xl md:text-5xl font-semibold mb-5 text-blue-900">
+              Nikmati pengalaman terbaik mengelola HR dan Bisnis Anda
+            </div>
+            <p className="text-lg md:text-2xl font-medium text-gray-400">
+              Dapatkan Akses ke Fitur dan Layanan Sesuai Keinginan Anda
+            </p>
+          </div>
+          <div className="mx-2 pb-36 md:flex md:flex-col md:justify-center md:items-center lg:pb-0 lg:flex lg:flex-row-reverse lg:gap-4 lg:mt-2">
             {/* gambar */}
             <img
-              src={GambarMainSaction}
-              alt="GambarMainSaction"
-              className="h-96 md:w-1/2 lg:mt-8 lg:h-auto"
+              src={GambarFitur}
+              alt="GambarFitur"
+              className="h-80 md:w-1/2 lg:mt-5 lg:h-auto"
             />
             {/* Pargraf dan tombol*/}
-            <div className="flex flex-col gap-5 mt-6 lg:w-1/2">
+            <div className="flex flex-col gap-5 mt-8 lg:w-1/2">
               {/* heading */}
               <div className="flex flex-col gap-4 lg:mb-5">
-                <q className="text-blue-900 text-xl font-bold text-center lg:text-5xl lg:mt-8 lg:mb-5 lg:text-start lg:leading-snug lg:font-semibold">
-                  Automasi payroll dan HRIS untuk pertumbuhan bisnis yang lebih
-                  cepat.
+                <q className="text-blue-900 text-lg font-medium text-center lg:text-2xl lg:mt-8 lg:mb-5 lg:text-start lg:leading-snug lg:font-medium">
+                  Fitur dan layanan Meraih
                 </q>
                 {keunggulan.map((item, index) => (
                   <div key={index}>
@@ -82,6 +85,16 @@ const FiturPage = () => {
               </div>
             </div>
           </div>
+        </div>
+
+        {/* Fitur */}
+        <div className="text-center mt-20">
+          <div className="text-3xl md:text-5xl font-semibold mb-5 text-blue-900">
+            Pelajari dan Coba Fitur Terbaik dari HRIS Meraih
+          </div>
+
+          {/* Border Box */}
+          
         </div>
       </div>
     </>
