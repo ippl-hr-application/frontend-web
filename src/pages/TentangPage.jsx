9c1e2dba9ce58303a5ae57063a420ebf68eb9244
MasRangga <ranggah503@gmail.com>
2024-03-13 12:23:38 +0800

    Done

--- src/pages/TentangPage.jsx
+++ src/pages/TentangPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { FaAngleRight } from "react-icons/fa";
 import GambarAbout from "../assets/GambarAbout.png";
 import Gambar3 from "../assets/Gambar3.png";
 import Footer from "../components/Footer";
@@ -61,7 +62,7 @@ const TentangPage = () => {
             </div>
             <button className="flex gap-1 items-center border-2 border-primary rounded-3xl py-2 px-4 bg-primary text-white hover:bg-blue-900 hover:border-blue-900 duration-300 lg:gap-2 lg:text-xl lg:font-medium lg:py-3 lg:px-6 mt-4 md:mt-0 ml-auto">
               <p>Hubungi Sales</p>
-              {/* <FaAngleRight className="w-5 h-5 lg:w-6 lg:h-6" /> */}
+              <FaAngleRight className="w-5 h-5 lg:w-6 lg:h-6" />
             </button>
           </div>
         </div>
