7c5f6568c68b2749521f3cb9466d1feff3563d45
MasRangga <ranggah503@gmail.com>
2024-03-13 10:51:11 +0800

    menambahkan PricingCard

---
+++ src/components/PricingCard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { ImWhatsapp } from "react-icons/im";
+
+const PricingCard = ({ imageSrc, title, text, features }) => {
+  return (
+    <div className="w-full md:w-80 bg-white rounded-xl shadow-xl p-6 mx-4 mb-8 transform transition duration-300 hover:scale-105">
+      <div className="relative mb-8">
+        <img src={imageSrc} alt={title} className="rounded-xl h-auto" />
+        <span className="absolute top-0 right-0 bg-yellow-500 text-white px-2 py-1 rounded-bl-xl text-sm font-medium">
+          {title}
+        </span>
+      </div>
+      <div className="mb-4">
+        <div className="text-justify">
+          <span className="text-gray-400 font-regular">{text}</span>
+        </div>
+        <div className="mt-5">
+          <button
+            className="border-2 rounded-3xl py-2 px-4 border-primary bg-white hover:bg-primary group duration-300 lg:py-3 lg:px-6"
+            data-aos="zoom-out"
+            data-aos-delay="200"
+          >
+            <a
+              href="https://wa.me/6285346901814"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 text-primary group-hover:text-white lg:gap-2 lg:text-xl lg:font-medium"
+            >
+              <ImWhatsapp className="w-5 h-5 lg:w-6 lg:h-6" />
+              <p>Hubungi Sales</p>
+            </a>
+          </button>
+        </div>
+      </div>
+      <ul className="text-gray-600 mb-4">
+        {features.map((feature, index) => (
+          <li key={index} className="flex items-center mb-2">
+            <BsFillCheckCircleFill className="text-primary bg-white w-6 h-6 border-2 rounded-full" />
+            {feature}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PricingCard;
--- src/pages/HargaPage.jsx
+++ src/pages/HargaPage.jsx
@@ -1,22 +1,70 @@
-import Navbar from "../components/Navbar"
+import React from "react";
+import Navbar from "../components/Navbar";
+import PricingCard from "../components/PricingCard"; // Mengimpor komponen PricingCard
+import GambarHargaBasic from "../assets/GambarHargaBasic.svg";
+import GambarHargaPro from "../assets/GambarHargaPro.svg";
+import GambarHargaMeraih460 from "../assets/GambarHargaMeraih460.svg";
+
 
 const HargaPage = () => {
-   return (
-     <>
-       <Navbar />
-       <div className="container pt-20 md:pt-44 mx-auto">
-         <div className="text-center">
-           <div className="text-3xl md:text-5xl font-semibold mb-5 text-blue-900">
-             Satu Solusi HR untuk Memenuhi Kebutuhan Operasional dan Strategis
-             Bisnis Anda dengan Sempurna
-           </div>
-           <p className="text-lg md:text-2xl font-medium text-gray-400">
-             Dapatkan Penawaran Terbaik yang Sesuai dengan Kebutuhan Bisnis Anda
-           </p>
-         </div>
-       </div>
-     </>
-   );
-}
+  return (
+    <>
+      <Navbar />
+      <div className="container pt-20 md:pt-44 mx-auto">
+        <div className="text-center">
+          <div className="text-3xl md:text-5xl font-semibold mb-5 text-blue-900">
+            Satu Solusi HR untuk Memenuhi Kebutuhan Operasional dan Strategis
+            Bisnis Anda dengan Sempurna
+          </div>
+          <p className="text-lg md:text-2xl font-medium text-gray-400">
+            Dapatkan Penawaran Terbaik yang Sesuai dengan Kebutuhan Bisnis Anda
+          </p>
+        </div>
 
-export default HargaPage
+        {/* Harga */}
+        <div className="flex flex-wrap justify-center mt-16">
+          <PricingCard
+            imageSrc={GambarHargaBasic}
+            title="Sistem HR Basic"
+            text="Sistem HR Praktis dengan Fitur Esensial untuk Meningkatkan Efisiensi dan Mengoptimalkan Proses SDM Anda"
+            features={[
+              "Get started with messaging",
+              "Flexible team meetings",
+              "5 TB cloud storage",
+            ]}
+          />
+          <PricingCard
+            imageSrc={GambarHargaPro}
+            title="Sistem HR Pro"
+            text="Sistem HR Tingkat Lanjut dengan Fitur Pendorong Produktivitas untuk Optimalisasi Operasional dan Kinerja Bisnis Anda"
+            features={[
+              "All features in Basic",
+              "Flexible call scheduling",
+              "15 TB cloud storage",
+            ]}
+          />
+          <PricingCard
+            imageSrc={GambarHargaMeraih460}
+            title="Sistem HR Meraih 460"
+            text="Solusi Terbaik untuk Bisnis dengan Kebutuhan Pengembangan Strategis dan Operasional yang Kompleks"
+            features={[
+              "All features in Startup",
+              "Growth oriented",
+              "Unlimited cloud storage",
+            ]}
+          />
+        </div>
+
+        {/* Newpage */}
+        <div className="text-center">
+          <div className="text-3xl md:text-5xl font-semibold my-8 text-blue-900">
+            Dirancang untuk Keseimbangan Fleksibilitas dan Transparansi yang
+            Optimal
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HargaPage;
